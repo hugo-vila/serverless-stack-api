a6179f51f7b27d83ff1aefc22827c14984f94fc7
Hugo Vila <hvila3@gmail.com>
2020-01-15 21:02:51 +0100

    feat: add [L]ist function

---
+++ list.js
@@ -0,0 +1,23 @@
+import * as dynamoDbLib from "./libs/dynamodb-lib";
+import { success, failure } from "./libs/response-lib";
+
+export async function main(event, context) {
+  const params = {
+    TableName: process.env.tableName,
+    KeyConditionExpression: "userId = :userId",
+    ExpressionAttributeValues: {
+      ":userId": event.requestContext.identity.cognitoIdentityId
+    }
+  };
+
+  try {
+    const result = await dynamoDbLib.call("query", params);
+    if (result.Items) {
+      return success(result.Items);
+    } else {
+      return failure({ status: false, error: "Items not found for User." });
+    }
+  } catch (e) {
+    return failure({ status: false });
+  }
+}
